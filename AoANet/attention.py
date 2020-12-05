87367c05b32e4f03df18eb6e8340812cb887323b
Gabriel <gabriel@gmail.com>
2020-12-04 23:30:24 -0300

    attention

--- AoANet/attention.py
+++ AoANet/attention.py
@@ -27,9 +27,9 @@ def decode_gts(data_gts, ix_to_word):
             else:
                 break
         sentences.append(text.strip())
-
     return sentences
-def eval_split(model, crit, loader, eval_kwargs={}):
+
+def eval_split(model, loader, eval_kwargs={}):
     verbose = eval_kwargs.get('verbose', True)
     num_images = eval_kwargs.get('num_images', eval_kwargs.get('val_images_use', -1))
     split = eval_kwargs.get('split', 'val')
@@ -92,3 +92,67 @@ def eval_split(model, crit, loader, eval_kwargs={}):
         if num_images >= 0 and n >= num_images:
             break
 
+import opts
+import models
+from dataloader import *
+from dataloaderraw import *
+import argparse
+import misc.utils as utils
+import torch
+
+# Input arguments and options
+parser = argparse.ArgumentParser()
+# Input paths
+parser.add_argument('--model', type=str, default='',
+                help='path to model to evaluate')
+parser.add_argument('--cnn_model', type=str,  default='resnet101',
+                help='resnet101, resnet152')
+parser.add_argument('--infos_path', type=str, default='',
+                help='path to infos to evaluate')
+parser.add_argument('--output_file', type=str, default='',
+                help='path to output')
+opts.add_eval_options(parser)
+
+opt = parser.parse_args()
+
+# Load infos
+with open(opt.infos_path, 'rb') as f:
+    infos = utils.pickle_load(f)
+
+# override and collect parameters
+replace = ['input_fc_dir', 'input_att_dir', 'input_box_dir', 'input_label_h5', 'input_json', 'batch_size', 'id']
+ignore = ['start_from']
+
+for k in vars(infos['opt']).keys():
+    if k in replace:
+        setattr(opt, k, getattr(opt, k) or getattr(infos['opt'], k, ''))
+    elif k not in ignore:
+        if not k in vars(opt):
+            vars(opt).update({k: vars(infos['opt'])[k]}) # copy over options from model
+
+vocab = infos['vocab'] # ix -> word mapping
+
+# Setup the model
+opt.vocab = vocab
+model = models.setup(opt)
+del opt.vocab
+model.load_state_dict(torch.load(opt.model))
+model.cuda()
+model.eval()
+
+# Create the Data Loader instance
+if len(opt.image_folder) == 0:
+  loader = DataLoader(opt)
+else:
+  loader = DataLoaderRaw({'folder_path': opt.image_folder,
+                            'coco_json': opt.coco_json,
+                            'batch_size': opt.batch_size,
+                            'cnn_model': opt.cnn_model})
+# When eval using provided pretrained model, the vocab may be different from what you have in your cocotalk.json
+# So make sure to use the vocab in infos file.
+loader.ix_to_word = infos['vocab']
+
+
+# Set sample options
+opt.dataset = opt.input_json
+eval_split(model, loader, vars(opt))
